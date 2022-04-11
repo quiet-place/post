9846879b8b1317df4b5f379a77d207df404b0d4b
otoolz <otool.hf@gmail.com>
2022-04-11 15:22:16 +0900

    created project

---
+++ ravenhongki/ravenhongki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ravenhongki
+//
+//  Created by KiWoong Hong on 2022/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ravenhongki/ravenhongki/ravenhongkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ravenhongkiApp.swift
+//  ravenhongki
+//
+//  Created by KiWoong Hong on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct ravenhongkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
